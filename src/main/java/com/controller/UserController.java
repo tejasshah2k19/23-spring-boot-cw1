56a28e7190c901a42cd38b18d11c4de87a06f944
tejas <tejasshah@gmail.com>
2023-10-04 20:59:27 +0530

    mix1

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.UserBean;
+
+@Controller
+public class UserController {
+
+	// create a method that is responsible to open signup jsp file
+	// in controller class method name we can take as per feature
+	// like for login -> login() , for signup -> signup()
+	// in controller your method must return String
+	// in controller your method must mark as public
+
+	@GetMapping("/signup")
+	public String signup() {
+		System.out.println("Signup()");
+		return "Signup"; // in the return we need pass jsp name that we want to open
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		System.out.println("login()");
+		return "Login";
+	}
+
+	// @Valid will check validation inside bean
+	// if error found it return error and erro flag to BindingResult
+	// using BindingResult we can get idea we have validation error or not ?
+
+	@PostMapping("/saveuser")
+	public String saveUser(@Valid @ModelAttribute("user") UserBean user, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
+			return "Signup";// error found re open Signup.jsp
+		} else {
+			System.out.println(user.getFirstName());
+			System.out.println(user.getEmail());
+			model.addAttribute("user", user);// set the name and value to send data to jsp
+//    	model.addAttribute("firstName",user.getFirstName());
+			return "Home";
+		}
+	}
+
+}
